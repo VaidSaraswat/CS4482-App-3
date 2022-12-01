0903ebe7deba6ae51d96a3566536e62fffb0df13
Edward <46017382+echang49@users.noreply.github.com>
2022-12-01 05:32:56 -0500

    Base main menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Unity.Netcode;
+using TMPro;
+
+public class MainMenu : MonoBehaviour
+{
+    public string lobbyScene;
+    public TMP_InputField hostNameInputField;
+    public TMP_InputField clientNameInputField;
+
+    public void HostGame()
+    {
+        PlayerPrefs.SetString("PlayerName", hostNameInputField.text);
+        NetworkManager.Singleton.StartHost();
+        NetworkManager.Singleton.SceneManager.LoadScene(lobbyScene, LoadSceneMode.Single);
+    }
+
+    public void JoinGame()
+    {
+        PlayerPrefs.SetString("PlayerName", clientNameInputField.text);
+        NetworkManager.Singleton.StartClient();
+    }    
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
