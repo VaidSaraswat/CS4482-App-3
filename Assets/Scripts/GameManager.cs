cc5f1908e15f87a0653b20998a8556f900744b1c
Edward <46017382+echang49@users.noreply.github.com>
2022-12-03 21:33:35 -0500

    Multiplayer movement pt. 2

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using Unity.Netcode;
 using TMPro;
@@ -9,7 +10,6 @@ public class GameManager : NetworkBehaviour
 {
     public GameObject PlayerPrefab;
     public GameObject VirtualCamera;
-    private GameObject player;
 
     //Inventory Variables
     public GameObject slotHolder;
@@ -17,25 +17,73 @@ public class GameManager : NetworkBehaviour
     public TMP_Text prompt;
     public TMP_Text pointDisplay;
 
+    private Dictionary<ulong, PlayerData> m_PlayerData = new Dictionary<ulong, PlayerData>();
+    private Dictionary<ulong, GameObject> m_Players = new Dictionary<ulong, GameObject>();
+
     void Start()
     {
-        player = Instantiate(PlayerPrefab);
+        m_PlayerData[NetworkManager.Singleton.LocalClientId] = new PlayerData(PlayerPrefs.GetString("PlayerName", "Default User"), 0);
+        SpawnPlayerServerRpc(NetworkManager.Singleton.LocalClientId);
+    }
+
+    void Update()
+    {
+        //m_PlayerData[NetworkManager.Singleton.LocalClientId].Score = ...
+        UpdatePlayerDataServerRpc(NetworkManager.Singleton.LocalClientId, m_PlayerData[NetworkManager.Singleton.LocalClientId]);
+    }
+
+    public GameObject GetPlayer()
+    {
+        return NetworkManager.LocalClient.PlayerObject.gameObject;
+    }
+
+    [ServerRpc(RequireOwnership=false)]
+    public void SpawnPlayerServerRpc(ulong clientId) {
+        m_Players[clientId] = Instantiate(PlayerPrefab);
+        m_Players[clientId].GetComponent<MovementManager>().playerId.Value = clientId;
+        m_Players[clientId].GetComponent<NetworkObject>().SpawnAsPlayerObject(clientId, true);
+        SetupCameraClientRpc(new ClientRpcParams {
+                Send = new ClientRpcSendParams
+                {
+                    TargetClientIds = new ulong[] { clientId }
+                }
+            });
+    }
+
+    [ClientRpc]
+    public void SetupCameraClientRpc(ClientRpcParams clientRpcParams) {
+        GameObject player = NetworkManager.LocalClient.PlayerObject.gameObject;
         VirtualCamera.GetComponent<CinemachineVirtualCamera>().Follow = player.transform.GetChild(0).transform;
         player.GetComponent<Inventory>().slotHolder = slotHolder;
         player.GetComponent<Inventory>().shop = shop;
         player.GetComponent<Inventory>().prompt = prompt;
         player.GetComponent<Inventory>().pointDisplay = pointDisplay;
-
-        SpawnPlayerServerRpc(NetworkManager.Singleton.LocalClientId);
-    }
-
-    public GameObject GetPlayer()
-    {
-        return player;
     }
 
     [ServerRpc(RequireOwnership=false)]
-    public void SpawnPlayerServerRpc(ulong clientId) {
-        player.GetComponent<NetworkObject>().SpawnAsPlayerObject(clientId, true);
+    public void UpdatePlayerDataServerRpc(ulong clientId, PlayerData data)
+    {
+        UpdatePlayerDataClientRpc(clientId, data);
     }
+
+    [ClientRpc]
+    public void UpdatePlayerDataClientRpc(ulong clientId, PlayerData data)
+    {
+        if(clientId != NetworkManager.Singleton.LocalClientId)
+        {
+            m_PlayerData[clientId] = data;
+        }
+    }
+}
+
+public struct PlayerData : INetworkSerializeByMemcpy
+{
+    public PlayerData(string name, int score)
+    {
+        Name = name;
+        Score = score;
+    }
+
+    public string Name;
+    public int Score;
 }
